453d32a0326583fb0a1661f5c84bdc5ef67f22e5
MarianoQga <106687324+MarianoQga@users.noreply.github.com>
2022-11-06 18:35:29 -0300

    Add files via upload

---
+++ GUIA_3 o 4_operaciones relacionales.py
@@ -0,0 +1,14 @@
+expresiones = [
+  False == True, 
+  10 >= 2*4,
+  33/3 == 11,
+  True > False,
+  True*5 == 2.5*2
+]
+#print(expresiones.index(True*5 == 2.5*2))
+
+print(str (expresiones[0]))
+print(str (expresiones[1]))
+print(str (expresiones[2]))
+print(str (expresiones[3]))
+print(str (expresiones[4]))
